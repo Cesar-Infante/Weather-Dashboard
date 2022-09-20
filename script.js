833ff5be5a33d896f82893b480567976db983010
cesar-infante <cesar261110@gmail.com>
2022-09-19 23:09:14 -0700

    Fixes to html, style changes to css, moment added for date and time

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
- const apiKey = '0ea29993b310996e906dcc76aebf0224';
+//api key
+const apiKey = '0ea29993b310996e906dcc76aebf0224';
+
+//DOM elements
+const timeEl = document.getElementById('time')
+const dateEl = document.getElementById('date')
+const currentWeatherItemsEl = document.getElementById('current-weather-items')
+const timeZone = document.getElementById('time-zone')
+const countryEl = document.getElementById('country')
+const weatherForecastEl = document.getElementById('weather-forecast')
+const currentTempEl = document.getElementById('current-temp')
+
+// using momentjs to set time and date
+dateEl.textContent = moment().format('dddd, MMMM Do, YYYY');
+timeEl.textContent = moment().format('hh:mm A');
+
