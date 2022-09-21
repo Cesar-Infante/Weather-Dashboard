e3b1e7700e4e7bae403dfca251d68dc0e8abbe0a
cesar-infante <cesar261110@gmail.com>
2022-09-20 22:55:20 -0700

    Proper implementation of geolocation, currently working on both chrome and safari

--- script.js
+++ script.js
@@ -2,59 +2,52 @@
 const apiKey = '0ea29993b310996e906dcc76aebf0224';
 
 //DOM elements
-const timeEl = document.getElementById('time')
-const dateEl = document.getElementById('date')
-const currentWeatherItemsEl = document.getElementById('current-weather-items')
-const timeZone = document.getElementById('time-zone')
-const countryEl = document.getElementById('country')
-const weatherForecastEl = document.getElementById('weather-forecast')
-const currentTempEl = document.getElementById('current-temp')
-
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timeZone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+const preloader = document.querySelector('.type-preloader')
 // using momentjs to set time and date
 dateEl.textContent = moment().format('dddd, MMMM Do, YYYY');
 timeEl.textContent = moment().format('hh:mm A');
 
-// function getWeatherData() {
-//   navigator.geolocation.getCurrentPosition((success) => {
+// Button to request weather
+const weatherBtn = document.getElementById('btn');
 
-//     let { latitude, longitude } = success.coords;
+// function to request location
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(useGeoLocation);
+  } else {
+    preloader.textContent = 'Denied Weather Request';
+  }
+}
 
-//     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`).then(res => res.json()).then(data => {
+// If request is successful, let latitude and longitude equal to success.coords and make a fetch request,
+// pass that data into showWeatherData
+function useGeoLocation(success) {
+  let { latitude, longitude } = success.coords;
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`).then(res => res.json()).then(data => {
 
-//       console.log(data)
-//       showWeatherData(data);
-//     })
+    // console.log(data);
+    showWeatherData(data);
+  });
+}
 
-//   })
-// }
+// event listener on weatherBtn
+weatherBtn.addEventListener('click', getLocation);
 
-// getWeatherData()
-
-const weatherBtn = document.getElementById('btn')
-
-weatherBtn.addEventListener('click', function () {
-  window.navigator.geolocation.getCurrentPosition((success, error) => {
-    if (error) {
-      console.log('User Denied Request')
-    } else {
-      let { latitude, longitude } = success.coords
-
-      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`).then(res => res.json()).then(data => {
-
-        console.log(data)
-        showWeatherData(data);
-      })
-    }
-  })
-});
-
-
-
+// the data that gets passed in will be destructured into the following variables and into current,
+// we then get timzone along with lat, lon
+// and pass the data into a template literal and utilizing saferInnerHTML to ensure security.
 const showWeatherData = (data) => {
-  let { humidity, uvi, sunrise, sunset, wind_speed } = data.current
+  let { humidity, uvi, sunrise, sunset, wind_speed } = data.current;
 
   timeZone.textContent = data.timezone;
-  countryEl.textContent = `${data.lat}N ${data.lon}E`
+  countryEl.textContent = `${data.lat}N ${data.lon}E`;
 
   const template = `
     <div class="weather-item">
@@ -67,7 +60,7 @@ const showWeatherData = (data) => {
     </div>
     <div class="weather-item">
       <div>Wind Speed</div>
-      <div>${wind_speed}</div>
+      <div>${wind_speed} mph</div>
     </div>
     <div class="weather-item">
       <div>Sunrise</div>
@@ -77,10 +70,11 @@ const showWeatherData = (data) => {
       <div>Sunset</div>
       <div>${window.moment(sunset * 1000).format('hh:mm a')}</div>
     </div>
-    `
-  saferInnerHTML(currentWeatherItemsEl, template)
+    `;
+  saferInnerHTML(currentWeatherItemsEl, template);
 
-  let otherDayForcast = ''
+  // template literal for each of the days following future forecast
+  let otherDayForecast = '';
   data.daily.forEach((day, idx) => {
     if (idx === 0) {
       const currentTemp = `
@@ -91,9 +85,9 @@ const showWeatherData = (data) => {
                 <div class="temp">Day - ${day.temp.day}&#176;F</div>
             </div>
             `
-      saferInnerHTML(currentTempEl, currentTemp)
+      saferInnerHTML(currentTempEl, currentTemp);
     } else {
-      const otherDaysTemp = otherDayForcast += `
+      const otherDaysTemp = otherDayForecast += `
             <div class="weather-forecast-item">
                 <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
                 <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
@@ -101,7 +95,7 @@ const showWeatherData = (data) => {
                 <div class="temp">Day - ${day.temp.day}&#176;F</div>
             </div>
             `
-      saferInnerHTML(weatherForecastEl, otherDaysTemp)
-    }
-  })
-}
+      saferInnerHTML(weatherForecastEl, otherDaysTemp);
+    };
+  });
+};
